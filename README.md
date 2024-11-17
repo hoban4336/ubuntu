0a6d761c531fa5787b9accf3b80a60be10aab90f
hoban4336 <hoban4336@gmail.com>
2024-11-17 19:50:32 +0900

    p10k configure

---
+++ README.md
@@ -0,0 +1,20 @@
+## command
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hoban4336/ubuntu/main/init.sh)"
+```
+
+[y](./resource/1.png)
+[n](./resource/2.png)
+[n](./resource/3.png)
+[3](./resource/4.png)
+[1](./resource/5.png)
+[2](./resource/6.png)
+[2](./resource/6.png)
+[1](./resource/7.png)
+[3](./resource/8.png)
+[3](./resource/9.png)
+[1](./resource/10.png)
+[1](./resource/11.png)
+[1](./resource/12.png)
+[n](./resource/13.png)
+[1](./resource/14.png)
--- init.sh
+++ init.sh
@@ -1,26 +1,138 @@
-#!/bin/bash
+#!/bin/sh
 
-# SSH 설치
-echo "Installing OpenSSH server..."
-sudo apt-get update -y
-sudo apt-get install -y openssh-server
+# 색상 정의
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # 색상 초기화
 
-# UFW 방화벽에서 SSH 포트 허용
-echo "Allowing SSH through the firewall..."
-sudo ufw allow ssh
-sudo ufw enable
+# 메시지 출력 함수
+print_step() { echo -e "${CYAN}$1${NC}"; sleep 1; }
+print_success() { echo -e "${GREEN}$1${NC}"; sleep 1; }
+print_error() { echo -e "${RED}Error: $1${NC}" >&2; exit 1; }
 
-# SSH 설정 변경: 공개 키 인증 활성화, 비밀번호 인증 비활성화
-echo "Configuring SSH settings..."
-sudo sed -i '/^#PubkeyAuthentication yes/s/^#//' /etc/ssh/sshd_config
-sudo sed -i 's/^#*PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+PUBLIC_KEY_URL="https://raw.githubusercontent.com/hoban4336/ubuntu/main/id_rsa.pub_hoban4336"
 
-# SSH 서비스 재시작
-echo "Restarting SSH service..."
-sudo systemctl restart sshd
+add_public_key_from_url() {
+  user_home=$1
+  print_step "Fetching public key from URL for user home: $user_home..."
 
-# SSH 서비스 상태 확인
-echo "Checking SSH service status..."
-sudo systemctl status sshd --no-pager
+  [ ! -d "$user_home/.ssh" ] && sudo mkdir -p "$user_home/.ssh" && sudo chmod 700 "$user_home/.ssh"
+  pub_key=$(curl -fsSL "$PUBLIC_KEY_URL") || print_error "Failed to fetch public key from URL"
 
-echo "SSH setup completed successfully."
+  # authorized_keys 파일에 키 추가
+  if ! grep -q "$pub_key" "$user_home/.ssh/authorized_keys" 2>/dev/null; then
+    echo "$pub_key" | sudo tee -a "$user_home/.ssh/authorized_keys" > /dev/null || print_error "Failed to add public key"
+    sudo chmod 600 "$user_home/.ssh/authorized_keys"
+    sudo chown -R "$NEW_USER:$NEW_USER" "$user_home/.ssh"
+    print_success "Public key added to $user_home/.ssh/authorized_keys successfully"
+  else
+    print_step "Public key already exists in $user_home/.ssh/authorized_keys"
+  fi
+}
+
+add_public_key() {
+  add_public_key_from_url "$HOME"
+}
+
+create_user() {
+  print_step "Creating a new user for Terraform..."
+  echo -n "Enter the username to create (default: terraformuser): "
+  read NEW_USER < /dev/tty
+  [ -z "$NEW_USER" ] && NEW_USER="terraformuser"
+  
+  if id "$NEW_USER" >/dev/null 2>&1; then
+    print_step "User $NEW_USER already exists, adding SSH key only..."
+    add_public_key_from_url "/home/$NEW_USER"
+  else
+    print_step "Creating user $NEW_USER..."
+    sudo adduser "$NEW_USER" --gecos "" --disabled-password || print_error "Failed to create $NEW_USER"
+    echo "$NEW_USER:password" | sudo chpasswd || print_error "Failed to set password for $NEW_USER"
+    sudo usermod -aG sudo "$NEW_USER" || print_error "Failed to add $NEW_USER to sudo group"
+    echo "$NEW_USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/"$NEW_USER" > /dev/null
+    print_success "User $NEW_USER created and configured successfully"
+    add_public_key_from_url "/home/$NEW_USER"
+  fi
+  sudo chown -R "$NEW_USER:$NEW_USER" "/home/$NEW_USER/.ssh"
+}
+
+configure_firewall() {
+  print_step "Configuring firewall for SSH..."
+  #SSH 포트
+  sudo ufw allow ssh    
+  sudo ufw --force enable
+  print_success "Firewall configured successfully"
+}
+
+configure_ssh() {
+  print_step "Configuring SSH settings..."
+  sudo sed -i '/^#PubkeyAuthentication yes/s/^#//' /etc/ssh/sshd_config || print_error "Failed to enable PubkeyAuthentication"
+  sudo sed -i 's/^#*PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config || print_error "Failed to disable PasswordAuthentication"
+  print_success "SSH settings configured successfully"
+  
+  print_step "Restarting SSH service..."
+  sudo systemctl restart sshd || print_error "Failed to restart SSH service"
+  sudo systemctl status sshd --no-pager
+  print_success "SSH service restarted successfully"
+}
+
+install_oh_my_zsh() {
+  print_step "Installing Oh My Zsh..."
+  rm -rf ~/.oh-my-zsh ~/.zsh* || print_error "Failed to clean up existing Zsh installations"
+  RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || print_error "Failed to install Oh My Zsh"
+  
+  print_step "Cloning Zsh plugins..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+  git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+  print_success "Oh My Zsh and plugins installed"
+}
+
+install_powerlevel10k() {
+  print_step "Installing Powerlevel10k font..."
+  wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+  wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+  mkdir -p ~/.fonts/ ~/.config/fontconfig/conf.d
+  mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+  mv PowerlineSymbols.otf ~/.fonts/
+  fc-cache -vf ~/.fonts/
+  
+  print_step "Installing Powerlevel10k theme..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" || print_error "Failed to install Powerlevel10k"
+  print_success "Powerlevel10k theme installed successfully"
+  
+  print_step "Configuring Powerlevel10k .zshrc file ..."
+  sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+  sed -i '/^plugins=(/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search zsh-completions)' ~/.zshrc
+  
+  print_success ".zshrc configured successfully"
+}
+
+install_dependencies() {
+  print_step "Installing dependencies..."
+  sudo apt-get update -y
+  sudo apt-get install -y openssh-server git curl vim zsh
+  print "\n"
+  print "\n"
+  
+  # Prerequisite 설정
+  print_step "Setting up default editor to vim..."
+  grep -q 'export EDITOR=vim' ~/.bashrc || echo 'export EDITOR=vim' >> ~/.bashrc
+  git config --global core.editor "vim"
+  print_success "Default editor set to vim"
+}
+
+main() {
+  install_dependencies
+  install_oh_my_zsh
+  install_powerlevel10k
+  add_public_key "$HOME"
+  create_user
+  configure_ssh
+  configure_firewall
+  print_success "All tasks completed!"
+}
+
+main
